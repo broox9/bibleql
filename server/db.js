c762f1c0581517f0ac83d9a10a23d6ac90f0772a
brookes stephens <broox9@gmail.com>
2018-01-12 01:30:19 -0500

    graphql server working

--- server/db.js
+++ server/db.js
@@ -1,12 +1,20 @@
 const { Client, Pool } = require('pg')
+const path = require('path')
+
+console.log('env', process.env.CREDENTIALS_PATH)
+const creds_path = path.join(process.env.CREDENTIALS_PATH, 'db.json')
+console.log('path', creds_path)
+const { bibleql } = require(creds_path)
+const dbcreds = process.env.NODE_ENV === "production" ? bibleql.aws : bibleql.local;
+const { host, pwd, user, db } = dbcreds
 
 // const opts = 'postgres://postgres:deploy@localhost:5432/bible_api'
 const opts = {
-  host: 'localhost',
-  user: 'postgres',
+  host,
+  user,
   port: 5432,
-  password: 'deploy',
-  database: 'bible_api',
+  password: pwd,
+  database: db,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000
 }
