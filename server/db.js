db3a7c3180c7b38d8c887e0e8b852f04a318253e
brookes stephens <broox9@gmail.com>
2018-01-15 11:42:49 -0500

    updates to DB info

--- server/db.js
+++ server/db.js
@@ -1,12 +1,13 @@
-const { Client, Pool } = require('pg')
-const path = require('path')
+const { Client, Pool } = require("pg");
+const path = require("path");
 
-console.log('env', process.env.CREDENTIALS_PATH)
-const creds_path = path.join(process.env.CREDENTIALS_PATH, 'db.json')
-console.log('path', creds_path)
-const { bibleql } = require(creds_path)
-const dbcreds = process.env.NODE_ENV === "production" ? bibleql.aws : bibleql.local;
-const { host, pwd, user, db } = dbcreds
+console.log("env", process.env.CREDENTIALS_PATH);
+const creds_path = path.join(process.env.CREDENTIALS_PATH, "db.json");
+console.log("path", creds_path);
+const { bibleql } = require(creds_path);
+const dbcreds =
+  process.env.NODE_ENV === "production" ? bibleql.aws : bibleql.local;
+const { host, pwd, user, db } = dbcreds;
 
 // const opts = 'postgres://postgres:deploy@localhost:5432/bible_api'
 const opts = {
@@ -17,17 +18,18 @@ const opts = {
   database: db,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000
-}
+};
 
-const pgClient = new Pool(opts)
+const pgClient = new Pool(opts);
 
-pgClient.connect()
+pgClient
+  .connect()
   .then(client => {
-    console.log('**pg client connected**')
+    console.log("**pg client connected** ~> env: ", process.env.NODE_ENV);
   })
   .catch(e => {
     console.log(e);
     throw new Error(e);
-  })
+  });
 
-module.exports = pgClient
+module.exports = pgClient;
